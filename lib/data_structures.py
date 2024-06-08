9e2e19b4a322035965791e8b3807b53a7a8a6cf0
gamze <gamzekandara@gmail.com>
2024-06-08 18:56:58 -0400

    Adds get_spiciest_foods function

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -21,67 +21,64 @@ spicy_foods = [
     },
 ]
 
-# this function takes a list of spicy_foods and returns a list of strings with the names.
 def get_names(spicy_foods):
     # using get() method and list comprehension
     return [food.get("name") for food in spicy_foods]
 
 
 get_names(spicy_foods)
-ipdb.set_trace()
 
 # takes a list , and returns a list of dictionaries for the food that has the heat_level greater than 5.
 def get_spiciest_foods(spicy_foods):
-    # iterate over the list to check heat_level properties
-    # return if heat_level > 5
-    # food.get("heat_level", 0) will return 0 if the "heat_level" key is not found in a dictionary, preventing a KeyError
-    pass
+    # return if heat_level > 5  => filter spicy foods.
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 get_spiciest_foods(spicy_foods)
+# ipdb.set_trace()
 
 
 # print(): Buffalo Wings (American) | Heat Level: 🌶🌶🌶
 # f string formatting to concatenate strings
 # * operator to multiply string emoji.
 # avoid conflicts with string literals and single quotes.
-def print_spicy_foods(spicy_foods):
+# def print_spicy_foods(spicy_foods):
 
 
-print_spicy_foods(spicy_foods)
+# print_spicy_foods(spicy_foods)
 
 
-# returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    # take list and cuisine string
+# # returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
+# def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+#     # take list and cuisine string
 
 
-get_spicy_food_by_cuisine(spicy_foods, "American")
+# get_spicy_food_by_cuisine(spicy_foods, "American")
 
 
-def print_spiciest_foods(spicy_foods):
-    # prints the spiciest foods heat_level > 5
-    # call get_spiciest_foods
-    # if heat leave > 5
+# def print_spiciest_foods(spicy_foods):
+#     # prints the spiciest foods heat_level > 5
+#     # call get_spiciest_foods
+#     # if heat leave > 5
 
 
-print_spiciest_foods(spicy_foods)
+# print_spiciest_foods(spicy_foods)
 
-# takes a list, and returns an integer =>average heat level
-
-
-def get_average_heat_level(spicy_foods):
+# # takes a list, and returns an integer =>average heat level
 
 
-print(get_average_heat_level(spicy_foods))
-
-def create_spicy_food(spicy_foods, spicy_food):
-    # spicy food is a dict
-    #returns the original list with new food added. 
+# def get_average_heat_level(spicy_foods):
 
 
-print(create_spicy_food(spicy_foods, {
-    'name': 'Griot',
-    'cuisine': 'Haitian',
-    'heat_level': 10,
-}))
+# print(get_average_heat_level(spicy_foods))
+
+# def create_spicy_food(spicy_foods, spicy_food):
+#     # spicy food is a dict
+#     #returns the original list with new food added. 
+
+
+# print(create_spicy_food(spicy_foods, {
+#     'name': 'Griot',
+#     'cuisine': 'Haitian',
+#     'heat_level': 10,
+# }))
 
