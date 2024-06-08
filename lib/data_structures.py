62f77b1ab6aebddd60677fc10bb3a8a6ca93281f
gamze <gamzekandara@gmail.com>
2024-06-08 19:23:50 -0400

    Adds data_structures.py

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,5 +1,4 @@
-# with given list of dictionaries representing different spicy foods.
-# use loops and list comprehensions alongside list and dict methods to solve these deliverables.
+import statistics
 import ipdb
 
 
@@ -21,6 +20,7 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
     # using get() method and list comprehension
     return [food.get("name") for food in spicy_foods]
@@ -29,56 +29,68 @@ def get_names(spicy_foods):
 get_names(spicy_foods)
 
 # takes a list , and returns a list of dictionaries for the food that has the heat_level greater than 5.
+
+
 def get_spiciest_foods(spicy_foods):
     # return if heat_level > 5  => filter spicy foods.
     return [food for food in spicy_foods if food["heat_level"] > 5]
+
 
 get_spiciest_foods(spicy_foods)
 # ipdb.set_trace()
 
 
 # print(): Buffalo Wings (American) | Heat Level: 🌶🌶🌶
-# f string formatting to concatenate strings
-# * operator to multiply string emoji.
-# avoid conflicts with string literals and single quotes.
-# def print_spicy_foods(spicy_foods):
+def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        print(
+            f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
 
 
-# print_spicy_foods(spicy_foods)
+print_spicy_foods(spicy_foods)
 
 
-# # returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
-# def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-#     # take list and cuisine string
+def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    # Filter out the list with matching cuisine.
+    x = [food for food in spicy_foods if food["cuisine"] == cuisine]
+    for food in x:
+        return food
 
 
-# get_spicy_food_by_cuisine(spicy_foods, "American")
+print(get_spicy_food_by_cuisine(spicy_foods, "American"))
 
 
-# def print_spiciest_foods(spicy_foods):
-#     # prints the spiciest foods heat_level > 5
-#     # call get_spiciest_foods
-#     # if heat leave > 5
+def print_spiciest_foods(spicy_foods):
+    # get spiciest foods.
+    spicy_foods = get_spiciest_foods(spicy_foods)
+    for food in spicy_foods:
+        print(
+            f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
 
 
-# print_spiciest_foods(spicy_foods)
-
-# # takes a list, and returns an integer =>average heat level
+print_spiciest_foods(spicy_foods)
 
 
-# def get_average_heat_level(spicy_foods):
+def get_average_heat_level(spicy_foods):
+    # Extract all heat levels from spicy_foods
+    heat_levels = [food.get("heat_level", 0) for food in spicy_foods]
+    # Calculate the average heat level using mean
+    average_heat_level = statistics.mean(heat_levels)
+
+    return average_heat_level
 
 
-# print(get_average_heat_level(spicy_foods))
-
-# def create_spicy_food(spicy_foods, spicy_food):
-#     # spicy food is a dict
-#     #returns the original list with new food added. 
+print(get_average_heat_level(spicy_foods))
 
 
-# print(create_spicy_food(spicy_foods, {
-#     'name': 'Griot',
-#     'cuisine': 'Haitian',
-#     'heat_level': 10,
-# }))
+def create_spicy_food(spicy_foods, spicy_food):
+    # Add the spicy_food to spicy_foods
+    spicy_foods.append(spicy_food)
+    return spicy_foods
 
+
+print(create_spicy_food(spicy_foods, {
+    'name': 'Griot',
+    'cuisine': 'Haitian',
+    'heat_level': 10,
+}))
